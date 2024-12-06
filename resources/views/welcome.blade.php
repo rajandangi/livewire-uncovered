cd73de94cf4d9d642f5eb79ba132da42507ec3bd
rajandangi <rajandangi649@gmail.com>
2024-12-06 20:10:21 +1030

    EP-08: Refactor custom livewire javascript functionality and add a feature to update html after response getting back

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,29 +13,7 @@
 
     @livewire(App\Http\Livewire\Counter::class)
 
-    <script>
-        // find all elements with wire:snapshot attribute
-        document.querySelectorAll('[wire\\:snapshot]').forEach(el => {
-            let snapshot = JSON.parse(el.getAttribute('wire:snapshot'));
-
-            el.addEventListener('click', e => {
-                if (!e.target.hasAttribute('wire:click')) return;
-
-                let method = e.target.getAttribute('wire:click');
-
-                fetch('/livewire', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({
-                        snapshot,
-                        callMethod: method,
-                    })
-                });
-            })
-        });
-    </script>
+    <script src="livewire.js"></script>
 </body>
 
 </html>
