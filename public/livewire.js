c8fd5c4c6e9fe6032b34e36158f07b9aca74a460
rajandangi <rajandangi649@gmail.com>
2024-12-13 12:45:17 +1030

    EP-14: Implement custom morph function for dynamic DOM updates

--- public/livewire.js
+++ public/livewire.js
@@ -66,8 +66,69 @@ function sendRequest(el, addToPayload) {
 
             el.__livewire = snapshot;
             // el.innerHTML = html;
-            Alpine.morph(el.firstElementChild, html);
+            morph(el.firstElementChild, html);
 
             updateWireModelInputs(el);
         })
+}
+
+// CUSTOM MORPH FUNCTION
+function morph(from, to) {
+    if (typeof to === 'string') {
+        let temp = document.createElement('div');
+        temp.innerHTML = to;
+        to = temp.firstElementChild;
+    }
+
+    if (from.tagName !== to.tagName) {
+        from.replaceWith(to.cloneNode(true));
+    }
+
+    patchText(from, to);
+    patchAttributes(from, to);
+    patchChildren(from, to);
+}
+
+function patchChildren(from, to) {
+    let childFrom = from.firstElementChild;
+    let childTo = to.firstElementChild;
+
+    // If there are more children in the to node, add them
+    while (childTo) {
+        if (!childFrom) {
+            childFrom = from.appendChild(childTo.cloneNode(true));
+        } else {
+            morph(childFrom, childTo);
+        }
+
+        childFrom = childFrom.nextElementSibling;
+        childTo = childTo.nextElementSibling;
+    }
+
+    // If there are more children in the from node, remove them
+    while (childFrom) {
+        let toRemove = childFrom;
+        childFrom = childFrom.nextElementsibling;
+        toRemove.remove();
+    }
+}
+
+function patchAttributes(from, to) {
+    // for (let attributes of to.attributes) {
+    //     from.setAttribute(attributes.name, attributes.value);
+    // }
+    for (let { name, value } of to.attributes) {
+        from.setAttribute(name, value);
+    }
+
+    for (let { name, value } of from.attributes) {
+        if (!to.hasAttribute(name)) {
+            from.removeAttribute(name)
+        }
+    }
+}
+
+function patchText(from, to) {
+    if (to.children.length > 0) return;
+    from.textContent = to.textContent;
 }
